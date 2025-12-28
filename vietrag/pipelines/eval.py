65de08e315d8f6c370befc56b33fddd419136d6d
DPTPhatUS <tuanphat51sp@gmail.com>
2025-12-28 13:52:52 +0700

    run config

--- vietrag/pipelines/eval.py
+++ vietrag/pipelines/eval.py
@@ -195,7 +195,7 @@ def run_ragas_eval(
             embeddings=embeddings,
             run_config=RunConfig(
                 max_workers=2,
-                timeout=900,
+                timeout=1500,
                 max_retries=1,
             ),
         )
